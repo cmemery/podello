26b2d9517356f086ebd8040c7bafb8044671a46d
Chris Emery <chris.emery@cpcc.edu>
2014-06-13 17:45:47 -0400

    ignore pyc files, add required settings to example settings file, add main function and some podio utility functions

--- script.py
+++ script.py
@@ -19,7 +19,6 @@ class TrelloCon(object):
     def get_card_details(self, card):
         card.fetch()
         return card
-        #print c.comments
 
     def get_checklists(self, card):
         if card.checklists > 1:
@@ -34,15 +33,28 @@ class PodioCon(object):
     def __init__(self, cid=client_id, cs=client_secret, uname=username, passwd=password):
         self.con = api.OAuthClient(cid,cs,uname,password)
 
-    def examples(self):
-        pass
+    def get_spaces(self, org_id):
+        return self.con.Space.find_all_for_org(org_id)
+
+    def get_space(self, urlfragment):
+        url = "%s/%s" % (url_root, urlfragment)
+        return self.con.Space.find_by_url(url)
+
+    def get_apps(self, space_id):
+        return self.con.Application.list_in_space(space_id)
+
+    def get_items(self, app_id):
+        items = self.con.Application.get_items(app_id)
+        for project in items['items']:
+            print project['title']
+
         #To create an item
         #item = {
         #	"fields":[
         #		{"external_id":"org-name", "values":[{"value":"The Items API sucks"}]}
         #	]
         #}
-        #print c.Application.find(179652)
+        #print c.Application.find()
         #c.Item.create(app_id, item)
         #Undefined and created at runtime example
         #print c.transport.GET.user.status()
@@ -55,3 +67,34 @@ class PodioCon(object):
         # application/x-www-form-urlencoded or application/json to match what API expects.
 
         #items[0]['fields'][2]['values'][0]['value']['file_id']
+
+if __name__ == "__main__":
+    def output_trello():
+        t = TrelloCon()
+        print 'Trello card data: \n'
+        for b in t.get_boards():
+            print ' Board: %s \n' % b.name
+            for l in t.get_lists(b):
+                print '   List: %s \n' % l.name
+                for c in t.get_cards(l):
+                    print '     Card: %s \n' % c.name
+        print 'Trello output complete'
+
+    def print_space(space):
+        print "%s \n" % space['name']
+        for k,v in space.iteritems():
+            print "%s: %s" % (k, v)
+
+    def output_podio():
+        p = PodioCon()
+        #spaces = p.get_spaces(org_id)
+        #for space in spaces:
+        #    print space
+        workspace_id = p.get_space(space_name)
+        apps = p.get_apps(workspace_id)
+        for app in apps:
+            if app['config']['name'] == 'Projects':
+                print "%d: %s" % (app['app_id'], app['config']['name'])
+
+    #output_trello()
+    output_podio()
